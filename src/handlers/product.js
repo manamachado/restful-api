9deaaa54a1799d60c5bc6d9aaa06f28f7947f8c8
Maná Machado <manasses.machado@luizalabs.com>
2019-11-02 12:23:52 -0300

    first :)

---
+++ src/handlers/product.js
@@ -0,0 +1,14 @@
+const ProductModel = require('../models/product');
+
+const getAll = (request, h) => {
+  return 'Hello World!';
+};
+
+const save = (req, h) => {
+  
+}
+
+module.exports = {
+  getAll,
+  save
+};
